4d945dedabd44e3db5c3fb101297349524c71470
RiyZ411 <riyanzaenal411@gmail.com>
2025-06-05 18:48:17 +0700

    Update repo

--- setup.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from sqlalchemy import create_engine
-
-df = pd.read_csv('./Dataset/data.csv', delimiter=';', index_col=False)
-
-URL = "postgresql://postgres.eagqpykvzctmtucgodic:p1pds.dicoding@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres"
- 
-engine = create_engine(URL)
-df.to_sql('students_performance', engine)
